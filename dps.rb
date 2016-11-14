f3ad9af7ff357ed75109044ffa8a8dacbf638b7e
Raúl Cruz <rcruzper@gmail.com>
2016-11-15 00:41:12 +0100

    update dps formula

--- dps.rb
+++ dps.rb
@@ -1,9 +1,9 @@
 class Dps < Formula
   desc "Command-line tool to execute most common actions with a docker container via docker ps and fzf"
   homepage "https://github.com/rcruzper/dps"
-  url "https://github.com/rcruzper/dps/archive/v1.0.1.tar.gz"
-  version "1.0.1"
-  sha256 "b07b4c3edb5bfab3ab7aec4cd5c94b2fca9a4369fe0850e44c581fdb59479a79"
+  url "https://github.com/rcruzper/dps/releases/download/v1.0.2/dps"
+  #version "1.0.2"
+  #sha256 "b07b4c3edb5bfab3ab7aec4cd5c94b2fca9a4369fe0850e44c581fdb59479a79"
 
   depends_on "fzf"
 
