14de95761afc785acac8161d9e170a569fe0128d
Travis CI <rcruzper@gmail.com>
2017-12-27 16:49:58 +0000

    Updated dps formula from TravisCI build 30

--- dps.rb
+++ dps.rb
@@ -1,9 +1,9 @@
 class Dps < Formula
   desc "Command-line tool to execute most common actions with a docker container via docker ps and fzf"
   homepage "https://github.com/rcruzper/dps"
-  url "https://github.com/rcruzper/dps/releases/download/v1.0.2/dps"
-  version "1.0.2"
-  sha256 "9a33d26faecfa23313bf4f5d6506376a84c46005d06816fd1ffe18b69c0fd7b1"
+  url "https://github.com/rcruzper/dps/releases/download/v1.0.3/dps"
+  version "1.0.3"
+  sha256 "83f48a3d0da3e40b34f898f7938e8743aa6a76d0b8bdbe0840006d2a38cd6dfe"
 
   depends_on "fzf"
 
