003ce6c8edf4247d6eb10a58848c82ae4ca11b08
Raúl Cruz <rcruzper@gmail.com>
2017-12-27 18:08:58 +0100

    fix: kps download url

--- kps.rb
+++ kps.rb
@@ -1,7 +1,7 @@
 class Kps < Formula
   desc "Command-line tool to execute most common actions with kubernetes pods via kubectl and fzf"
   homepage "https://github.com/rcruzper/kps"
-  url "https://github.com/rcruzper/kps/releases/download/v1.0.1/kps"
+  url "https://github.com/rcruzper/kps/releases/download/v1.0.9/kps"
   version "1.0.9"
   sha256 "3ab09934979210461aef8c68a20543fc95a3c9e0e3549c8cfea7c3ff264888bc"
 
